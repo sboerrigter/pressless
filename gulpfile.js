27c55c3eb0ea66aadea2e36df27c77e2aae2a3e0
Sjoerd Boerrigter <sjoerd@trendwerk.nl>
2017-06-13 22:56:24 +0200

    :sparkles: browserSync

--- gulpfile.js
+++ gulpfile.js
@@ -1,26 +1,43 @@
 'use strict';
 
-const gulp = require('gulp');
+const gulp         = require('gulp');
 const autoprefixer = require('gulp-autoprefixer');
-const sass = require('gulp-sass');
-const sassGlob = require('gulp-sass-glob');
-const scssLint = require('gulp-scss-lint');
+const browserSync  = require('browser-sync');
+const sass         = require('gulp-sass');
+const sassGlob     = require('gulp-sass-glob');
+const scssLint     = require('gulp-scss-lint');
+
+const stylesDir    = './assets/styles';
+const publicDir    = './public';
 
 gulp.task('scssLint', () => {
-  gulp.src('assets/styles/**/*')
+  gulp.src(stylesDir + '/**/*.scss')
     .pipe(scssLint({
       'config': 'config/scss-lint.yml'
     }));
 });
 
 gulp.task('sass', () => {
-  gulp.src('assets/styles/main.scss')
+  gulp.src(stylesDir + '/main.scss')
     .pipe(sassGlob())
     .pipe(sass({outputStyle: 'compressed'}))
     .pipe(autoprefixer())
-    .pipe(gulp.dest('public'));
+    .pipe(gulp.dest(publicDir))
 });
 
-gulp.task('watch', () => {
-  gulp.watch('./assets/styles/**/*.scss', ['scssLint', 'sass']);
+gulp.task('serve', function() {
+  browserSync.init({
+    server: publicDir,
+  });
+});
+
+gulp.task('reload', function() {
+  browserSync.reload();
+});
+
+gulp.task('default', () => {
+  gulp.run('serve');
+
+  gulp.watch(stylesDir + '/**/*.scss', ['scssLint', 'sass', 'reload']);
+  gulp.watch(publicDir + '/**/*.html', ['reload']);
 });
