5a5442f615aed18054a46bd44e8993fd0b9ab41d
Sjoerd Boerrigter <sjoerd@trendwerk.nl>
2017-06-16 14:51:21 +0200

    :art: gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -7,18 +7,18 @@ const sass         = require('gulp-sass');
 const sassGlob     = require('gulp-sass-glob');
 const scssLint     = require('gulp-scss-lint');
 
-const stylesDir    = './assets/styles';
-const publicDir    = './public';
+const stylesDir    = './assets/styles/';
+const publicDir    = './public/';
 
 gulp.task('scssLint', () => {
-  gulp.src(stylesDir + '/**/*.scss')
+  gulp.src(stylesDir + '**/*.scss')
     .pipe(scssLint({
       'config': 'config/scss-lint.yml'
     }));
 });
 
 gulp.task('sass', ['scssLint'], () => {
-  gulp.src(stylesDir + '/main.scss')
+  gulp.src(stylesDir + 'main.scss')
     .pipe(sassGlob())
     .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
     .pipe(autoprefixer())
@@ -36,6 +36,6 @@ gulp.task('reload', ['scssLint', 'sass'], function() {
 });
 
 gulp.task('watch', ['serve'], () => {
-  gulp.watch(stylesDir + '/**/*.scss', ['scssLint', 'sass', 'reload']);
-  gulp.watch(publicDir + '/**/*.html', ['reload']);
+  gulp.watch(stylesDir + '**/*.scss', ['scssLint', 'sass', 'reload']);
+  gulp.watch(publicDir + '**/*.html', ['reload']);
 });
