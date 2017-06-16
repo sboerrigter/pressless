58bf382665e4696609f99f89ae0836918bffa38d
Sjoerd Boerrigter <sjoerd@trendwerk.nl>
2017-06-16 15:16:38 +0200

    :config: Webpack transpile with babel and minify output

--- webpack.config.js
+++ webpack.config.js
@@ -4,5 +4,25 @@ module.exports = {
   entry: './assets/scripts/main.js',
   output: {
     filename: './public/script.js'
-  }
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js/,
+        loader: 'babel-loader',
+        query: {
+          compact: true,
+          presets: ['es2015']
+        }
+      }
+    ],
+  },
+  plugins: [
+    new webpack.optimize.UglifyJsPlugin({
+      output: {
+        comments: false
+      }
+    }),
+  ],
+  watch: false
 }
