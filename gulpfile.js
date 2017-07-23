290c62c95f4655a7fd4a7eeea31efac117cbd122
Sjoerd Boerrigter <sjoerd@trendwerk.nl>
2017-07-23 20:42:26 +0200

    :wrench: Dont stop gulp on webpack error

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,27 @@
 'use strict';
 
-const gulp         = require('gulp');
-const autoprefixer = require('gulp-autoprefixer');
-const browserSync  = require('browser-sync').create();
-const sass         = require('gulp-sass');
-const sassGlob     = require('gulp-sass-glob');
-const scssLint     = require('gulp-scss-lint');
-const webpack      = require('webpack-stream');
+const gulp          = require('gulp');
+const autoprefixer  = require('gulp-autoprefixer');
+const browserSync   = require('browser-sync').create();
+const sass          = require('gulp-sass');
+const sassGlob      = require('gulp-sass-glob');
+const scssLint      = require('gulp-scss-lint');
+const webpack       = require('webpack-stream');
 
-const scriptsDir   = './assets/scripts/';
-const stylesDir    = './assets/styles/';
-const publicDir    = './public/';
+const scriptsDir    = './assets/scripts/';
+const stylesDir     = './assets/styles/';
+const publicDir     = './public/';
+
+const webpackConfig = require('./webpack.config.js');
+
+function webpackError() {
+  this.emit('end');
+}
 
 gulp.task('js', () => {
   gulp.src(scriptsDir + 'main.js')
-    .pipe(webpack(require('./webpack.config.js')))
+    .pipe(webpack(webpackConfig))
+    .on('error', webpackError)
     .pipe(gulp.dest(publicDir))
     .pipe(browserSync.stream());
 });
@@ -29,7 +36,9 @@ gulp.task('scssLint', () => {
 gulp.task('sass', ['scssLint'], () => {
   gulp.src(stylesDir + 'main.scss')
     .pipe(sassGlob())
-    .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+    .pipe(sass({outputStyle: 'compressed'})
+      .on('error', sass.logError)
+    )
     .pipe(autoprefixer())
     .pipe(gulp.dest(publicDir))
     .pipe(browserSync.stream());
