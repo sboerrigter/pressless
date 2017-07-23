740d7b43d293ca4aa8e5a7856ef666bd18639a3d
Sjoerd Boerrigter <sjoerd@trendwerk.nl>
2017-07-23 23:13:46 +0200

    :hammer: Navigation as Vue component

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,29 +1,52 @@
 import Vue from 'vue';
 
-new Vue({
-  el: '.header',
-  data: {
-    sitename: 'PressLess',
-    isOpen: false,
-    menu: [
-      {
-        label: 'Home',
-        link: '#',
-      },
-      {
-        label: 'Blog',
-        link: '#',
-      },
-      {
-        label: 'About',
-        link: '#',
-      },
-      {
-        label: 'Contact',
-        link: '#',
-      },
-    ],
+Vue.component('navigation', {
+  template: `
+    <header class="header container" :class="{ 'open': isOpen }">
+      <div class="wrapper">
+        <a class="logo" href="#">{{ sitename }}</a>
+
+        <div class="hamburger" @click="toggleMenu">
+          <div class="hamburger__line"></div>
+          <div class="hamburger__line"></div>
+          <div class="hamburger__line"></div>
+          <div class="hamburger__line"></div>
+        </div>
+
+        <nav class="menu">
+          <navigation-item class="menu__item" :href="item.link" v-for="item in items">
+            {{ item.label }}
+          </navigation-item>
+        </nav>
+      </div>
+    </header>
+  `,
+
+  data() {
+    return {
+      sitename: 'PressLess',
+      isOpen: false,
+      items: [
+        {
+          label: 'Home',
+          link: '#',
+        },
+        {
+          label: 'Blog',
+          link: '#',
+        },
+        {
+          label: 'About',
+          link: '#',
+        },
+        {
+          label: 'Contact',
+          link: '#',
+        },
+      ],
+    };
   },
+
   methods: {
     toggleMenu() {
       if (this.isOpen) {
@@ -34,3 +57,17 @@ new Vue({
     },
   },
 });
+
+Vue.component('navigation-item', {
+  template: `
+    <a class="menu__item">
+      <slot></slot>
+    </a>
+  `,
+});
+
+const app = new Vue({
+  el: '#app',
+});
+
+app();
